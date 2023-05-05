f000c4273646c5bab567cf7a3472a70decb0a222
Evgeny Larin <118234834+Evgeny-Larin@users.noreply.github.com>
2023-05-05 14:02:22 +0700

    Add files via upload

---
+++ maplegend.py
@@ -0,0 +1,184 @@
+import folium
+from folium import plugins
+import pandas as pd
+import streamlit as st
+
+
+#заменяет полное название компании на сокращенное
+def replacer(x):
+    #словарь замен
+    replace_dict = {'ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ': 'ООО',
+                    'ПУБЛИЧНОЕ АКЦИОНЕРНОЕ ОБЩЕСТВО': 'ПАО',
+                    'ОТКРЫТОЕ АКЦИОНЕРНОЕ ОБЩЕСТВО': 'ОАО',
+                    'АКЦИОНЕРНОЕ ОБЩЕСТВО': 'АО'}
+    for old, new in replace_dict.items():
+        x = x.upper().replace(old, new)
+    return x
+
+#определяет Топ13 компаний
+#принимает df с точками внутри региона (например, p_kem)
+def points_rating(x):
+    #выделим ТОП-12 по количеству наиболее крупных сетей, остальные сохраним в Прочие
+    #посчитаем количество точек в той или иной сети
+    points_count = x.groupby('name', as_index = False)\
+                    .agg({'lat':'count'})\
+                    .sort_values('lat', ascending = False)
+
+    #назначим ранги для каждой сети, где 1 - самая крупная
+    points_count['rank'] = points_count['lat'].rank(method = 'first', ascending = False)
+
+    #сохраним отдельно ТОП-12 сетей
+    top12 = points_count[(points_count['rank']<=12)].drop(columns = ['rank'])
+    
+    #сохраним отдельно прочие
+    other_points = points_count[(points_count['rank']>12)].drop(columns = ['name'])
+    #создадим столбец "Прочие"
+    other_points['name'] = 'ПРОЧИЕ'
+    #найдём суммарное количество точек всех прочих сетей
+    other_points = other_points.groupby('name', as_index = False)\
+                               .agg({'lat':'sum'})
+    
+    #добавляем прочих к топ-12
+    top13 = pd.concat([top12, other_points])
+   
+    #создаем столбец для легенды
+    top13['labels'] = top13['name'] + " — " + top13['lat'].astype('str')
+
+    #переименовываем столбец с количеством точек
+    top13.rename(columns = {'lat':'all_count'}, inplace = True)
+    return top13
+
+#принимает df с топ13 компаний и установленную пользователем hex палитру
+#присваивает каждой компании соответсвующий цвет, возвращает словарь {компания:цвет}
+def color_distributor(top13, hex_palette):
+    #берём название всех компаний в топе
+    companies = top13.name.tolist()
+    #удаляем ПРОЧИЕ
+    try:
+        companies.remove('ПРОЧИЕ')
+    except:
+        pass
+    #присваиваем компаниям цвета из палитры
+    comp_n_colors = {}
+    x = 0
+    for i in companies:
+        comp_n_colors[i] = [hex_palette[x]]
+        x += 1    
+        
+    return comp_n_colors
+
+#генератор карты
+#принимает df с координатами точек в столбцах lat и lon
+def map_creator(df, mapstyle):
+    russia_map = folium.Map(location = [df.lat.median(), df.lon.median()], #начальная позиция камеры
+                            zoom_start = 7, #начальный зум камеры
+                            tiles = None) #стиль карты None, чтобы не было названия в легенде
+    
+    #задачем стиль карты
+    if mapstyle == 'Стандартная':
+        cartodbpositron_map(russia_map)
+    elif mapstyle == 'ЖД пути и станции':
+        cartodbpositron_map(russia_map)
+        openrailway_map(russia_map)
+    else:
+        thunderforestrail_map(russia_map)  
+
+    
+    #добавляем плагины
+    #полноэкранный режим
+    plugins.Fullscreen(
+    position="bottomleft",
+    force_separate_button=True).add_to(russia_map)
+    #поиск
+    plugins.Geocoder(collapsed=True, position='topleft').add_to(russia_map)
+    #миникарта
+    minimap = plugins.MiniMap()
+    russia_map.add_child(minimap)
+    return russia_map
+
+def cartodbpositron_map(russia_map):
+        folium.TileLayer(
+        tiles="cartodbpositron",
+        attr='<a href=_</a>',
+        max_zoom=19,
+        name='darkmatter',
+        control=False,
+        opacity=0.7).add_to(russia_map)
+        
+def openrailway_map(russia_map):
+        folium.TileLayer(
+        tiles="https://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png",
+        attr='<a href=_</a>',
+        max_zoom=19,
+        name='darkmatter',
+        control=False,
+        opacity=0.7).add_to(russia_map)
+
+def thunderforestrail_map(russia_map):
+        folium.TileLayer(
+        tiles="https://tile.thunderforest.com/transport/{z}/{x}/{y}.png?apikey=32828de45b164f449366abd6a45999ef",
+        attr='<a href="http://www.thunderforest.com/">Thunderforest</a>',
+        max_zoom=19,
+        name='darkmatter',
+        control=False,
+        opacity=0.7).add_to(russia_map)    
+        
+
+#добавляет города на карту
+#принимает df с базой городов и карту, на которую нужно добавить города
+def city_creator(city_db, russia_map):
+    return city_db.apply(lambda row:folium.Circle(location=[row["Latitude"], row["Longitude"]], #координаты города
+                                                    radius=(row["Population"]/60 if row["Population"]/50>6000 else 6000), #радиус окружности
+                                                    tooltip=row['CityName'], #всплывающая подсказка
+                                                    fill=True, #настройка заполнения цветом
+                                                    weight=1,  #настройка прозрачности и т.д.
+                                                    fill_opacity=0.02).add_to(russia_map), axis=1)
+
+#добавляет точки на карту
+#принимает df с координатами необходимых точек, карту, на которую нужно добавить города, палитру цветов
+def points_creator(points, russia_map, hex_palette):
+    #определяем топ13 компаний и прочие
+    top13 = points_rating(points)
+    #добавляем к основному df признак топовости компании, остальных помечаем как Прочие
+    points_region = pd.merge(points,
+                             top13,
+                             how = 'left',
+                             on = 'name')\
+                      .sort_values('all_count', ascending = False)
+
+    try:
+        points_region['labels'].fillna(top13[top13['name'] == 'ПРОЧИЕ']['labels'][0], inplace = True)
+    except:
+        pass
+    
+    #распределяем цвета из hex палитры среди топ13 компаний
+    comp_n_colors = color_distributor(top13, hex_palette)
+    #группируем по подписи легенды
+    for grp_name, df_grp in points_region.groupby(['labels'], sort = False):
+        #берём соответсвующее имя компании
+        name = df_grp['name'].unique()[0]
+        try:
+            #получаем цвет компании
+            color = comp_n_colors[name][0]
+        except:
+            #если цвет не нашли - цвет серый
+            color = 'grey'
+        #объединяем в группы, устанавливаем цвет и название группы 
+        feature_group = folium.FeatureGroup(f'<span style="color: {color};"> ⬤ {grp_name}</span>')
+        #для каждой строки в группе
+        for row in df_grp.itertuples():
+            #рисуем точку. row3 и row4 - координаты, row2 - адрес точки
+            folium.Circle(location=[row[3], row[4]], 
+                                         radius=90,
+                                         popup=repr(row[2]),
+                                         tooltip=row[1], 
+                                         fill = True, 
+                                         weight=0.5, 
+                                         color = 'black', 
+                                         weight_color = 'black', 
+                                         fill_opacity=1, 
+                                         fill_color=color).add_to(feature_group)
+        feature_group.add_to(russia_map)
+
+    #добавляем панель управления группами (легенду) на карту
+    folium.map.LayerControl('topright', collapsed= False).add_to(russia_map)
---
+++ streamlit_app.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+# !python -m pip --trusted-host=pypi.org --trusted-host=files.pythonhosted.org install --default-timeout=100 --upgrade streamlit-folium
+# !python -m pip --trusted-host=pypi.org --trusted-host=files.pythonhosted.org install --default-timeout=100 --upgrade streamlit
+# !python -m pip --trusted-host=pypi.org --trusted-host=files.pythonhosted.org install --default-timeout=100 --upgrade folium
+import streamlit as st
+from streamlit_folium import st_folium
+from maplegend import *
+
+#содержимое страницы по ширине (по умолчанию по центру)
+st.set_page_config(page_title = 'Map Generator', page_icon = '🗺️', layout="wide")
+
+#загружаем файл с координатами
+file = st.file_uploader('Загрузите файл Excel с координатами точек:', type = ['.xlsx', '.xlsm', '.xls', '.xlsb'])
+
+#образец файла для пользователя
+with st.expander("Пример исходной таблицы"):
+    st.image("https://i.ibb.co/c8DJ9ZP/f3580096-358a-4ab2-b07b-d41895789bd9.jpg")
+
+regions_list = []
+if file != None:
+    points = pd.read_excel(file)
+    points.rename(columns = {'Наименование':'name',
+                             'Адрес':'address',
+                             'Широта': 'lat',
+                             'Долгота':'lon',
+                             'Город':'city',
+                             'Регион':'SubRegion'}, inplace = True)
+    points = points[(~points.lat.isna())&(~points.lon.isna())]
+    #выбор интересующих регионов или все на одной карте
+    values = ['Все регионы на одной карте']
+    values.extend(points.SubRegion.drop_duplicates().tolist())
+    regions_list = st.multiselect('Выберите регион', values)
+
+    #если выбраны конкретные регионы - отбираем только подходящие точки
+    if "Все регионы на одной карте" not in regions_list:
+        points = points[points.SubRegion.isin(regions_list)]
+
+
+col01, col02 = st.columns(2)
+
+#задаем настройки карты
+with st.sidebar:
+    st.write("Настройка карты:")
+    mapstyle = st.radio(
+                "Стиль:",
+                ('Стандартная', 'ЖД пути и станции', 'ЖД пути и станции 2'))
+    #если пользователь хочет отображать города на карте
+    city_on = st.checkbox('Отображать города на карте')
+    point_size = st.slider('Размер точек', 50, 200, 90)
+
+    #настройка легенды
+    st.write("Настройка легенды:")
+    #создаем таблицу 3х4 для цветов
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+       hex1 = st.color_picker('Цвет 1', '#980387', key = 1)
+       hex5 = st.color_picker('Цвет 5', '#7f9c21', key = 2)
+       hex9 = st.color_picker('Цвет 9', '#f12121', key = 3)
+    with col2:
+       hex2 = st.color_picker('Цвет 2', '#ff9000', key = 4)
+       hex6 = st.color_picker('Цвет 6', '#441066', key = 5)
+       hex10 = st.color_picker('Цвет 10', '#00d7e6', key = 6) 
+    with col3:
+       hex3 = st.color_picker('Цвет 3', '#f70068', key = 7)
+       hex7 = st.color_picker('Цвет 7', '#f15821', key = 8)
+       hex11 = st.color_picker('Цвет 11', '#E600D7', key = 9)
+    with col4:
+       hex4 = st.color_picker('Цвет 4', '#2970e2', key = 10)
+       hex8 = st.color_picker('Цвет 8', '#64E600', key = 11)
+       hex12 = st.color_picker('Цвет 12', '#9383C9', key = 12)
+       
+#сохраняем выбранные цвета
+hex_palette = [hex1,hex2,hex3,hex4,hex5,hex6,hex7,hex8,hex9,hex10,hex11,hex12]
+
+
+#если пользователь отмечает города и выбрал какой-либо регион
+if city_on and regions_list != []:
+    #подгружаем базу городов
+    city_db = pd.read_csv(r'https://raw.githubusercontent.com/Evgeny-Larin/OnlineMaps_Folium/main/db/cities_db.csv',usecols=['CityName', 'SubRegion', 'Latitude', 'Longitude', 'Population'], encoding='windows-1251', sep = ';')
+    #из базы берём только необходимые города
+    city_db = city_db[city_db['CityName'].isin(points.city.drop_duplicates().tolist())]
+
+
+#если выбрано "Все регионы на одной карте" - строим одну карту
+if regions_list ==  ["Все регионы на одной карте"]:
+           
+    #создаем карту
+    russia_map = map_creator(points, mapstyle)
+    
+    #если отмечаем города на карте - отмечаем их на карте    
+    if city_on:
+        city_creator(city_db, russia_map)
+
+    #отмечаем точки на карте    
+    points_creator(points, russia_map, hex_palette)
+    
+    #преобразовываем объект карты (russia_map) в HTML-строку
+    map_html = russia_map.get_root().render()
+    
+    #создаем кнопку для загрузки файла 
+    file_name = "Общая карта.html"
+    button = st.download_button(label=f"Сохранить карту: {file_name}", data=map_html, file_name=file_name)
+    
+    #выводим предпросмотр карты на страницy
+    st_folium(russia_map,
+              height=500,
+              width=1200)
+
+#если выбраны конкретные регионы - строим карту для каждого региона
+else:
+    for i in regions_list:
+        #если пользователь выбрал конкретные регионы И опцию "Все регионы на одной карте"
+        #для "Все регионы на одной карте" - берем основной df
+        if i ==  "Все регионы на одной карте":
+            points_region = points.copy()
+        #для регионов - выбираем из основного df нужные регионы
+        else:
+            points_region = points[(points['SubRegion'] == i)]            
+    
+        #создаем карту
+        russia_map = map_creator(points_region, mapstyle)
+
+        #если отмечаем города на карте - отмечаем их на карте
+        if city_on:
+            city_db = city_db.query(f'SubRegion == "{i}"')
+            city_creator(city_db, russia_map)
+        
+        #отмечаем точки на карте
+        points_creator(points_region, russia_map, hex_palette)
+        
+        #преобразовываем объект карты (russia_map) в HTML-строку
+        map_html = russia_map.get_root().render()
+        
+        #создаем кнопку для загрузки файла 
+        file_name = f"{i} - карта.html"
+        button = st.download_button(label=f"Сохранить карту: {i}", data=map_html, file_name=file_name, key = i)
+        
+        #выводим предпросмотр карты на страницy
+        st_folium(russia_map,
+                  height=500,
+                  width=1200,
+                  key = i)
